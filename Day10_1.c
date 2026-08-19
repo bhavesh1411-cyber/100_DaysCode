1663cb3490a82a84a4aa4c6ce2d6c2150279e40e
Bhavesh <gbhavesh1411@gmail.com>
2026-08-19 04:38:32 +0000

    update projects

---
+++ Day10_1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("Enter the lenght of a:");
+    scanf("%d", &a);
+
+    printf("Enter the lenght of b:");
+    scanf("%d", &b);
+
+    printf("Enter the lenght of c:");
+    scanf("%d", &c);
+
+    if (a == b && b == c)
+    {
+        printf("It is an Equilateral Triangle\n");
+    }
+    else if (a * a + b * b == c * c || b * b + c * c == a * a || c * c + a * a == b * b)
+    {
+        printf("It is an Right Angled Triangle\n");
+    }
+    else if (a == b || b == c || c == a)
+    {
+        printf("It is an Isosceles Triangle\n");
+    }
+    else
+    {
+        printf("It is an Scalene Triangle\n");
+    }
+
+    return 0;
+}
